882dfe950f07caf09eedd56794d2ee4095583e70
gpratoe <gaston.prato@mi.unc.edu.ar>
2024-11-03 02:20:15 -0300

    sounds v2

--- code/sounds.py
+++ code/sounds.py
@@ -8,32 +8,34 @@ class Sounds:
 
         self.sounds = {"single": None,
                        "composite": [],
-                       "c6": mixer.Sound("sounds/c6.mp3"),
-                       "d6": mixer.Sound("sounds/d6.mp3"),
-                       "e6": mixer.Sound("sounds/e6.mp3"),
-                       "f6": mixer.Sound("sounds/f6.mp3"),
-                       "g6": mixer.Sound("sounds/g6.mp3"),
-                       "a6": mixer.Sound("sounds/a6.mp3")}
+                       "notes":{}
+                        }
+
+        notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+        for i in range (5, 7):
+            for note in notes:
+                self.sounds["notes"][note + str(i)] = mixer.Sound("sounds/notes/vibraphone/" + note + str(i) + ".wav")
+        self.sounds["notes"]["C7"] = mixer.Sound("sounds/notes/vibraphone/C7.wav")
+
 
         for i in range(1, 12):
             self.sounds["composite"].append(mixer.Sound("sounds/pingpongdeluxe-" + str(i) + ".wav"))
 
-        self.song = ["c6", "c6", "d6", "c6", "f6", "e6", #hbd song
-                     "c6", "c6", "d6", "c6", "g6", "f6",
-                     "c6", "c6", "a6", "f6",
-                     "f6", "f6", "e6", "d6",
-                     "a6", "a6", "g6", "e6", "g6", "f6"]
+        self.song = ["C6", "C7", "B6", "G6", "A6", "B6",
+                     "C7", "C6", "A6", "G6", "A5", "F6", 
+                     "E6", "C6", "D6", "E6", "F6", "D6", 
+                     "B5", "C6", "D6", "E6", "C6"]
         
         self.next_note = 0
         self.next_note_comp = 0
 
     def play_song(self):
-        for sound in self.sounds.values():
+        for sound in self.sounds["notes"].values():
             sound.stop()
         
-        note = self.sounds[self.song[self.next_note]]
-        self.next_note = (self.next_note + 1) % self.song.__len__()
-        note.play()
+        self.sounds["notes"][self.song[self.next_note]].play()
+        self.next_note = (self.next_note + 1) % len(self.song)
+        
 
 
     def play_composite(self):
@@ -41,13 +43,12 @@ class Sounds:
             sound.stop()
 
         self.sounds["composite"][self.next_note_comp].play()
-        self.next_note_comp = (self.next_note_comp + 1) % self.sounds["composite"].__len__()
+        self.next_note_comp = (self.next_note_comp + 1) % len(self.sounds["composite"])
 
     def set_sound(self, sound_path):
         self.sounds["single"] = mixer.Sound(sound_path)
     
     def play_single_sound(self):
         self.sounds["single"].stop()
-        self.sounds["single"].set_volume(ri(70, 100)/100)
         self.sounds["single"].play()    
         
